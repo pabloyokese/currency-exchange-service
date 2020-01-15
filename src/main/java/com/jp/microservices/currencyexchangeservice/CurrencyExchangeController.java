ea2a1af7eb4f1a9888e6b6b44c2ed9b5bf7e4396
juan <oqundo.juan@system-in-motion.com>
2020-01-15 17:53:11 +0800

    first commit

---
+++ src/main/java/com/jp/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,27 @@
+package com.jp.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    ExchangeValueRespository exchangeValueRespository;
+
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to){
+        ExchangeValue exchangeValue = exchangeValueRespository.findByFromAndTo(from, to);
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        return exchangeValue;
+    }
+}
---
+++ src/main/java/com/jp/microservices/currencyexchangeservice/ExchangeValueRespository.java
@@ -0,0 +1,7 @@
+package com.jp.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRespository extends JpaRepository<ExchangeValue, Long> {
+    ExchangeValue findByFromAndTo(String from, String to);
+}
